ee26956c002a15b5484ec0898804152012c413aa
HarshParikh0810 <gujaratiking39@gmail.com>
2025-04-08 15:02:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 allowed_origins = os.getenv('ALLOWED_ORIGINS', 'http://localhost:5500').split(',')
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
